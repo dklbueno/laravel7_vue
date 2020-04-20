ae2cac8e9558e69c5b8d6906377ea79a0b7faf3c
Daniel Bueno <dkbueno@gmail.com>
2020-04-20 10:11:40 -0300

    blog

---
+++ blog/resources/js/components/Pagina.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="container">
+      <div class="row">
+          <div v-bind:class="defineTamanho">
+              <slot></slot>
+          </div>
+      </div>
+  </div>
+</template>
+<script>
+    export default {
+        props:['tamanho'],
+        computed:{
+            defineTamanho: function(){
+                if(this.tamanho >= 12){
+                    return "col-md-12";
+                }
+                if(this.tamanho <= 2){
+                    return "col-md-2 offset-md-5";
+                }
+                if(this.tamanho % 2 > 0){
+                    this.tamanho ++;
+                }
+                return "col-md-"+this.tamanho+" offset-md-"+(12-this.tamanho)/2;
+            }
+        }
+    }
+</script>
